07a2de307c458e141e71ee43235082c82e4d6e98
Mikael <mantism@seas.upenn.edu>
2019-01-14 01:45:48 -0500

    Adds script.js for nav bar, starts other sections

---
+++ src/script.js
@@ -0,0 +1,113 @@
+var isInView = function (element) {
+  var elementTop = element.offset().top;
+  var elementBot = elementTop + element.outerHeight();
+  var screenBot = $(window).scrollTop() + $(window).height();
+  return (screenBot > elementTop) && (screenBot <  elementBot);
+}
+
+homeBottom = $('#home').offset().top + $('#home').outerHeight()
+navButtons = ['#home-button', '#gathering-button', '#about-button', '#media-button', '#merch-button', '#contact-button']
+var toggleActive = function (element) {
+  for (var i = 0; i < navButtons.length; i++) {
+    if (element === navButtons[i]) {
+      $(navButtons[i]).addClass('active');
+    } else {
+      $(navButtons[i]).removeClass('active');
+    }
+  }
+}
+
+
+var yearButtonClick = function(div) {
+  if (div.id === 'button-2018') {
+    $('#button-2018').addClass('selected')
+    $('#button-2019').removeClass('selected')
+    $('#v1').addClass('active')
+    $('#v2').removeClass('active')
+  } else {
+    $('#button-2019').addClass('selected')
+    $('#button-2018').removeClass('selected')
+    $('#v2').addClass('active')
+    $('#v1').removeClass('active')
+  }
+}
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() < homeBottom) {
+    toggleActive('#home-button');
+    $('header').css('background-color', '');
+    atTop = true;
+  } else {
+    $('header').css('background-color', '#1c1c1c');
+    atTop = false;
+  }
+
+  if (isInView($('#gathering')) && !atTop) {
+    toggleActive('#gathering-button');
+  }
+
+  if (isInView($('#about'))) {
+    toggleActive('#about-button');
+  }
+
+  if (isInView($('#media'))) {
+    toggleActive('#media-button');
+  }
+
+  if (isInView($('#merch'))) {
+    toggleActive('#merch-button')
+  }
+
+  if (isInView($('#contact'))) {
+    toggleActive('#contact-button')
+  }
+});
+
+$(document).ready( function() {
+  $('#home-button').click(function() {
+    toggleActive('#home-button');
+  });
+
+  $('#gathering-button').click(function() {
+    toggleActive('#gathering-button');
+  });
+
+  $('#about-button').click(function() {
+    toggleActive('#about-button');
+  });
+
+  $('#media-button').click(function() {
+    toggleActive('#media-button');
+  });
+
+  $('#merch-button').click(function() {
+    toggleActive('#merch-button');
+  })
+
+  $('#contact-button').click(function() {
+    toggleActive('#contact-button')
+  })
+
+  /*$("a[href^='#']").click(function(e) {
+    if (this.hash !== ' ') {
+      e.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 50
+      }, 800, function() {
+        window.location.hash = hash;
+      });
+    }
+  });*/
+
+  $(".topnav").click(function(e) {
+    var x = document.getElementById("links");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  })
+});

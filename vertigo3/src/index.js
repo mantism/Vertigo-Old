7c9da1568a8f28a7e1a61ccd4489060d26758500
Mikael <mantism@seas.upenn.edu>
2019-12-04 22:10:07 -0500

    adds material-ui package, changes app.tsx and header.tsx to app.jsx and header.jsx

--- vertigo3/src/index.js
+++ vertigo3/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/app/App.tsx';
+import App from './components/app/App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
